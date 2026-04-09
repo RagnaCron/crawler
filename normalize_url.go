8d1ee3b0c02c07670ce4a15a85a08ddf36138579
Manuel Werder <manuelwerder@hotmail.com>
2026-04-09 23:20:35 +0200

    missed the error message

--- normalize_url.go
+++ normalize_url.go
@@ -9,7 +9,7 @@ import (
 func normalizeURL(u string) (string, error) {
 	sURL, err := url.Parse(u)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
 	}
 
 	return fmt.Sprintf("%s%s", sURL.Host, strings.TrimSuffix(sURL.Path, "/")), nil
